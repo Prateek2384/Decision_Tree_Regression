997bb6fe9188bf8c787d1b8499b4ae36e25f12ad
Prateek Swami <118006355+Prateek2384@users.noreply.github.com>
2024-07-04 23:26:03 +0530

    decision_tree_regression.py

--- decision_tree_regression.py
+++ decision_tree_regression.py
@@ -1,24 +1,17 @@
-# Decision Tree Regression
-
-# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
-# Training the Decision Tree Regression model on the whole dataset
 from sklearn.tree import DecisionTreeRegressor
 regressor = DecisionTreeRegressor(random_state = 0)
 regressor.fit(X, y)
 
-# Predicting a new result
 regressor.predict([[6.5]])
 
-# Visualising the Decision Tree Regression results (higher resolution)
 X_grid = np.arange(min(X), max(X), 0.01)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
